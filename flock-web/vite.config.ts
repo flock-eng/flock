2392444391c13636f5e939d99676ef5c8958f96d
Will Cygan <wcygan.io@gmail.com>
2025-01-12 13:01:05 -0600

    fiddling with versions

--- flock-web/vite.config.ts
+++ flock-web/vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit()
+	],
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
