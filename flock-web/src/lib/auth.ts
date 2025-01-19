9c66606336010d261cf739452ce1ddef59e1cdd0
Will Cygan <wcygan.io@gmail.com>
2025-01-19 17:16:00 -0600

    signout invalidates keycloak session

--- flock-web/src/lib/auth.ts
+++ flock-web/src/lib/auth.ts
@@ -1,5 +1,13 @@
 import NextAuth from "next-auth"
 import KeycloakProvider from "next-auth/providers/keycloak"
+import { JWT } from "next-auth/jwt"
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id_token?: string;
+    provider?: string;
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -15,11 +23,33 @@ export const {
     }),
   ],
   callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.id_token = account.id_token
+        token.provider = account.provider
+      }
+      return token
+    },
     async session({ session, token }) {
       if (session.user) {
-        session.user.id = token.sub
+        session.user.id = token.sub!
       }
       return session
+    },
+  },
+  events: {
+    async signOut(message) {
+      if ('token' in message && message.token && 
+          'provider' in message.token && message.token.provider === "keycloak" && 
+          'id_token' in message.token && message.token.id_token) {
+        const issuerUrl = process.env.KEYCLOAK_ISSUER!
+        const logOutUrl = new URL(`${issuerUrl}/protocol/openid-connect/logout`)
+        console.log("Signing out", message.token.id_token)
+        console.log("Issuer URL", issuerUrl)
+        console.log("Log out URL", logOutUrl)
+        logOutUrl.searchParams.set("id_token_hint", message.token.id_token)
+        await fetch(logOutUrl)
+      }
     },
   },
   pages: {
