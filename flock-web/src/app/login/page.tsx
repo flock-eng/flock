17a8f0054b602e45f9ba959693242748fbdeadec
Will Cygan <wcygan.io@gmail.com>
2025-01-20 11:43:22 -0600

    add a button

--- flock-web/src/app/login/page.tsx
+++ flock-web/src/app/login/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 
+import { Button } from "@/components/ui/button"
+
 export default function LoginPage() {
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="text-center">
         <h1 className="text-4xl font-bold">Please login to continue!</h1>
+        <Button onClick={() => console.log('Button clicked!')}>Click me</Button>
       </div>
     </main>
   )
