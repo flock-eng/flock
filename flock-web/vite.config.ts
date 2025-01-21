72e1c430d7a22c8af0e598fd146332015bec366b
Will Cygan <wcygan.io@gmail.com>
2025-01-21 02:17:46 -0600

    serve on port 3000

--- flock-web/vite.config.ts
+++ flock-web/vite.config.ts
@@ -3,7 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
+	server: {
+		port: 3000
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
