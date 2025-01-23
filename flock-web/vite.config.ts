898bdfed1df2c2fcd0807754aa239ae38760d1b2
Will Cygan <wcygan.io@gmail.com>
2025-01-22 23:19:03 -0600

    make sure the site is accessible

--- flock-web/vite.config.ts
+++ flock-web/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		port: 3000
-	},
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [sveltekit()],
+    server: {
+      port: 3000,
+      host: true,
+      allowedHosts: [env.VITE_PUBLIC_APP_DOMAIN]
+    },
+    test: {
+      include: ['src/**/*.{test,spec}.{js,ts}']
+    }
+  };
 });
